f4a7a7196170b5673815358df3019e74071ab502
Carlos Gomez <carlos.gomglz@gmail.com>
2024-01-13 20:46:06 -0600

    Second Commit

---
+++ views/auth/login.php
@@ -0,0 +1,33 @@
+<main class="auth">
+    <h2 class="auth__heading"><?php echo $titulo ?></h2>
+    <p class="auth__texto">Inicia sesion en DevWebCamp</p>
+
+    <form class="formulario">
+       <div class="formulario__campo">
+            <label class="formulario__label">Email</label>
+            <input 
+                type="text"
+                class="formulario__input"
+                placeholder="Tu Email"
+                id="email"
+                name="email"
+            >
+       </div> 
+       <div class="formulario__campo">
+            <label class="formulario__label">Password</label>
+            <input 
+                type="password"
+                class="formulario__input"
+                placeholder="Tu Password"
+                id="password"
+                name="password"
+            >
+       </div> 
+       <input type="submit" class="formulario__submit" value="Iniciar Sesion">
+    </form>
+
+    <div class="acciones">
+        <a href="/registro" class="acciones__enlace">¿Aun no tienes cuenta? Obtener una</a>
+        <a href="/olvide" class="acciones__enlace">¿Olvidaste tu password?</a>
+    </div>
+</main>
