f1684f1418fe2e37ae9577b588c776396151e396
Carlos Gomez <carlos.gomglz@gmail.com>
2024-02-17 12:13:15 -0600

    Agregando clase de Paginacion

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -18,12 +18,12 @@ class PonentesController {
             header('Location: /admin/ponentes?page=1');
         }
         
-        $registros_por_pagina = 10;
+        $registros_por_pagina = 5;
 
         $total = Ponente::total();
         $paginacion = new Paginacion($pagina_actual, $registros_por_pagina, $total);
 
-        debuguear($paginacion);
+        debuguear($paginacion->pagina_siguiente());
 
         if(!is_admin()){
             header('Location: /login');
